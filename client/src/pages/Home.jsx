c8a6b867db93e6c1a4d19b5851471a6637d738c5
Victor Maduanusi <maduanusivictor2020@gmail.com>
2024-08-07 10:45:56 +0100

    useNavigate hook

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,12 @@
 // import React from 'react'
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
   const [listOfPost, setListOfPost] = useState([]);
+  const navigate = useNavigate();
+
   useEffect(() => {
     axios.get("http://localhost:3001/post").then((response) => {
       setListOfPost(response.data);
@@ -13,7 +16,13 @@ function Home() {
     <div>
       {listOfPost.map((value, key) => {
         return (
-          <div className="post" key={key}>
+          <div
+            className="post"
+            key={key}
+            onClick={() => {
+              navigate(`post/${value.id}`);
+            }}
+          >
             <div className="title">{value.title}</div>
             <div className="body">{value.postText}</div>
             <div className="footer">{value.username}</div>
