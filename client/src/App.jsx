8b7534df76c7b8f44554a3f9b36dfb5d1d5eb940
Victor Maduanusi <maduanusivictor2020@gmail.com>
2024-08-07 10:00:15 +0100

    modified:   client/src/App.css 	modified:   client/src/App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,18 +5,17 @@ import CreatePost from "./pages/CreatePost";
 import Post from "./pages/Post";
 function App() {
   return (
-    <>
-      <h1>Hello React!</h1>
-      <div>
-      <Link to='/createPost'>Create Post</Link><br />
-      <Link to='/'>Home</Link>
-        <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="/createPost" element={<CreatePost />}/>
-          <Route path="/post/:id" element={<Post />}/>
-        </Routes>
+    <div className="App">
+      <div className="navbar">
+        <Link to='/'>Home</Link>
+        <Link to='/createPost'>Create Post</Link><br />
       </div>
-    </>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/createPost" element={<CreatePost />}/>
+        <Route path="/post/:id" element={<Post />}/>
+      </Routes>
+    </div>
   );
 }
 
